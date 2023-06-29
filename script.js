e0db17994a85a5267832a4e944aac93bdb0e5eec
Emileny <119755388+EmilenyRochaLeal@users.noreply.github.com>
2023-06-29 11:15:51 -0300

    Mais alterações no script e no htlml
    No script foi implementada alguns funções de adionar e visualizar. No html foi alterado a class do span para um id

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let inputdesc = document.querySelector("#descGanho");
 let inputGanho = document.querySelector('#valorSalario');
 let saidaDesc = document.querySelector('#desc');
 let saidaPreco = document.querySelector('#preco');
-let p1 = document.getElementById('p1');
 let butao = document.getElementById('bnt');
+let bntAdicionar = document.querySelector('#butaoAdicionar')
 let dados = new Array();
-let total = 0;
-
+let despesas = new Array();
+let somaEntradas;
+let valorGanho;
+let gasto;
+let somaSaidas;
+let total;
 function verificar(){
     if (localStorage.hasOwnProperty("dados")){
         dados = JSON.parse(localStorage.getItem("dados"));
@@ -15,10 +19,58 @@ function verificar(){
 function adicionar(){
     dados.push({descricao: inputdesc.value, preco: inputGanho.value})
     localStorage.setItem("dados", JSON.stringify(dados))
-
 }
-
-butao.addEventListener("click", () => {
-    verificar()
-    adicionar()
-});
+function calcularEntradas(){
+    somaEntradas = 0
+    for (let i  in dados){
+        if (dados.hasOwnProperty(i)){
+            valorGanho = parseFloat(dados[i].preco);
+            if (!isNaN(valorGanho)){ 
+                dados[i].preco = valorGanho;
+                somaEntradas += valorGanho;
+        }else{
+            dados[i].preco = 0
+        }}
+    }
+}
+function guardar(){
+    if (localStorage.hasOwnProperty("despesas")){
+        despesas = JSON.parse(localStorage.getItem("despesas"));
+    }
+    despesas.push({descricao: saidaDesc.value, preco: saidaPreco.value})
+    localStorage.setItem("despesas", JSON.stringify(despesas))
+}
+function calcularSaidas(){
+    somaSaidas = 0
+    for (let i  in despesas){
+        if (despesas.hasOwnProperty(i)){
+            gasto = parseFloat(despesas[i].preco);
+            if (!isNaN(gasto)){ 
+                despesas[i].preco = gasto;
+                somaSaidas += gasto;
+            }else{
+                despesas[i].preco = 0
+            }}
+        }
+    }
+    function analise(){
+        total = somaEntradas - somaSaidas;
+        document.querySelector("#Saida").textContent = somaSaidas.toFixed(2);
+        document.querySelector('#entrada').textContent = somaEntradas.toFixed(2);
+        document.querySelector("#total").textContent = total.toFixed(2);
+    }
+    butao.addEventListener("click", () => {
+        verificar()
+        adicionar()
+        calcularEntradas()
+        inputGanho.value = '';
+        inputdesc.value = '';
+        analise()
+    });
+    bntAdicionar.addEventListener("click",()=> {
+        guardar()
+        calcularSaidas()
+        analise()
+        saidaDesc.value = '';
+        saidaPreco.value = '';
+    })
